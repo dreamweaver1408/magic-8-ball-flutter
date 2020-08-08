9a76dce5e1da100752b84b5fdd5757979ea42060
dreamweaver1408 <as@0xb8.de>
2020-08-08 16:56:13 +0200

    Magic Ball App finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+          ),
+          body: Magic(),
+        ),
       ),
     );
+
+class Magic extends StatefulWidget {
+  @override
+  _MagicState createState() => _MagicState();
+}
+
+class _MagicState extends State<Magic> {
+  int ballNumber = 1;
+
+  void shakeBall() {
+    setState(
+      () {
+        ballNumber = Random().nextInt(5) + 1;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        FlatButton(
+          onPressed: () {
+            shakeBall();
+          },
+          child: Image.asset('./images/ball$ballNumber.png'),
+        ),
+      ],
+    );
+  }
+}
